70972a940160af4b2f7eef40fa12a9e49e881dde
Steven Chae <52509659+isoseven@users.noreply.github.com>
2025-08-28 20:21:03 -0400

    asdasd
    asdasdasd

--- rivergreen-ap/src/main/java/com/stkych/rivergreenap/DatabaseConfig.java
+++ rivergreen-ap/src/main/java/com/stkych/rivergreenap/DatabaseConfig.java
@@ -4,8 +4,8 @@ package com.stkych.rivergreenap;
  * Configuration values for connecting to the database.
  */
 public class DatabaseConfig {
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/opendental?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    public static final String DB_URL = "jdbc:mysql://rgserver:3306/opendental?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "test";
+    public static final String DB_PASSWORD = "password";
 }
 
